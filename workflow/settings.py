496ee2d5fcd95e6d0854e355684d4d686414cf24
v4rd <anoncrazyer@protonmail.com>
2021-08-22 22:39:18 +1000

    add main page

--- workflow/settings.py
+++ workflow/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'js'),
+    os.path.join(BASE_DIR, 'css')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
